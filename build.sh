16fbbb73e0e67cb23d4ab7116117c302180876ab
Traffic <trafficconegod@gmail.com>
2021-08-04 13:35:10 -0400

    change app to scc

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir build
 cd build
 g++ -I ../Source -c $(find ../Source -name '*.cpp') -std=c++17 -fpermissive $1
-g++ -I ../Source $(find . -name '*.o') -o app $1
+g++ -I ../Source $(find . -name '*.o') -o scc $1
 cd ..
